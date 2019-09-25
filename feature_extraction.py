e5be378bd40accafb2097751722a590ce3a83ba8
Thales <thalesteng@gmail.com>
2019-09-25 10:47:43 -0300

    Base for split and merge algorithm

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,71 +1,63 @@
 import cv2
 import numpy as np
 
-def main():
-    #print(np.array([2, 1, 2]))
-    data = np.load('ExtractedPoints.npy')
-    #print(len(data[0]))
+def split(x_coord, y_coord, left_idx, right_idx, canvas):
+    if right_idx <= left_idx:
+        return
 
+    distance = np.zeros(shape=(1, len(x_coord)), dtype=np.float32)
+    norm_dist = np.zeros_like(distance)
+
+    #lines = cv2.HoughLinesP(plot_arr, 0.5, (np.pi/180.0), 100000)
+    A = np.vstack([x_coord, np.ones(len(x_coord))]).T
+    m, c = np.linalg.lstsq(A, y_coord, rcond=None)[0]
+    print(m, c)
+    print("Min y:", (np.min(x_coord*m) + c), "max y:", (np.max(x_coord*m) + c))
+    denom = np.sqrt((m**2) + 1)
+    x_0 = int((1024/14)*(np.min(x_coord) + 6))
+    y_0 = 1024 - int((1024/14)*((np.min(x_coord*m) + c) + 8))
+    x_f = int((1024/14)*(np.max(x_coord) + 6))
+    y_f = 1024 - int((1024/14)*((np.max(x_coord*m) + c) + 8))
+    print("y_0:", y_0, "y_f:", y_f)
+    cv2.line(canvas, (x_0, y_0), (x_f, y_f), (255, 0, 0), 3)
+
+    for idx, element in enumerate(x_coord):
+        distance[0][idx] = np.abs((m * element) + y_coord[idx] + c)/denom
+    norm_dist = [distance/np.max(distance)]
+    print(norm_dist)
+
+
+def main():
+    data = np.load('ExtractedPoints.npy')
+
+    data = data*(-1)
+ 
     points = np.zeros(shape=(len(data[0]), 2), dtype=np.float32)
     for i, _ in enumerate(points):
         points[i] = [data[0][i], data[1][i]]
-        #print(i, points[i])
-    #points = [np.array([x, y], dtype=np.float32) for x, y in zip(data[0], data[1])]
-    
-    print(data[0].min(), data[0].max())
-    print(data[1].min(), data[1].max())
 
-    plot_arr = np.full((1024, 1024), 255,dtype=np.uint8)
+    print("Mix x:", np.min(data[0]), "Max x:", np.max(data[0]))
+
+    plot_arr = np.full((1024, 1024, 3), 255, dtype=np.uint8)
     result = np.full((1024, 1024, 3), 255, dtype=np.uint8)
     #print(points[0][0], points[0][1])
     for point in points:
-        x = int((1024/14)*((-1)*point[0] + 6))
-        y = 1024 - int((1024/14)*((-1)*point[1] + 8))
+        x = int((1024/14)*(point[0] + 6))
+        y = 1024 - int((1024/14)*(point[1] + 8))
         #print(x, y)
-        plot_arr[y][x] = 0
+        plot_arr[y][x][0] = 0
+        plot_arr[y][x][1] = 0
+        plot_arr[y][x][2] = 0
         #print(x, y)
 
-    cv2.imwrite("test_mono.png", plot_arr)
     points = points.reshape((-1, 1, 2))
 
-    intersection_points = []
-    #lines = cv2.HoughLinesP(plot_arr, 0.5, (np.pi/180.0), 100000)
-    lines = cv2.HoughLinesPointSet(points, 15, 100000, 0, 10, 0.5, 0, (np.pi/2.0), (np.pi/180.0))
-    for l in lines:
-        for line in l:
-            votes, rho, theta = line
-            print(votes, "rho:", rho, "theta:", theta)
+    split(data[0], data[1], 0, len(data[0]), plot_arr)
+    #print("intersection_points")
 
-            if (theta == 0):
-                x0 = rho
-                x1 = rho
-                y0 = -6
-                y1 = 8
-            else:
-                x0 = -8
-                x1 = 6
-                y0 = (rho - x0*np.cos(theta))/np.sin(theta)
-                y1 = (rho - x1*np.cos(theta))/np.sin(theta)
-            print(x0, y0, x1, y1)
-
-            # a = np.cos(theta)
-            # b = np.sin(theta)
-            # x0 =a*rho
-            # y0 = b*rho
-            #x0 = int((1024/14)*(((-1)*a*rho) + 6))#a*rho
-            #y0 = 1024 - int((1024/14)*(((-1)*b*rho) + 8))#b*rho
-            #cv2.line(result, (x0, y0), (x0 + 100, y0), (0, 0, 255), 3)
-            #print(x0, y0)
-            intersection_points.append((x0, y0))
-
-    for i_p in intersection_points:
-        x = int((1024/16)*(i_p[0] + 6))
-        y = 1024 - int((1024/16)*(i_p[1] + 8))
-        result[y][x-10:x+10][:] = 0
-    #print(intersection_points)
-
-    #cv2.imshow("Window", result)
-    #cv2.waitKey(0)
+    cv2.imshow("Window", plot_arr)
+    cv2.waitKey(0)
+    cv2.imwrite("features.png", plot_arr)
 
 if __name__ == '__main__':
     main()
